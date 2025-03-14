e1723d5a7193b2295620d52e7e938b618e37efeb
TheLuck12 <150361035+TheLuck12@users.noreply.github.com>
2025-03-14 20:21:08 -0300

    agregue barra de proceso

--- Scripts/interfaz.gd
+++ Scripts/interfaz.gd
@@ -1,12 +1,19 @@
 extends CanvasLayer
-
-func _process(delta: float) -> void:
-	if Global.enJuego == false:
-		$Timer.autostart = false
-
-
+@onready var Barra = $CanvasGroup/BarraProceso
+func _ready() -> void:
+	$AnimationPlayer.play("Barra")
+func _process(_delta: float) -> void:
+	DetenerTimer()
 func _on_timer_timeout() -> void:
 	Global.timer += 1
-	$ProgressBar.value += 0.6
-	$Label.text = str(Global.timer)
-	
+	Barra.value += 1
+	print(Global.timer)
+
+
+func DetenerTimer():
+	if Barra.value == 60:
+		$Timer.stop()
+		$AnimationPlayer.pause()
+	elif Global.enJuego == false:
+		$Timer.stop()
+		$AnimationPlayer.pause()
--- Scripts/mov_obs_3.gd
+++ Scripts/mov_obs_3.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Enemigo"):
+	if area.is_in_group("Enemigo3"):
 		area.Movimiento = 0
 
 
 func _on_area_2d_2_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Enemigo"):
+	if area.is_in_group("Enemigo3"):
 		area.Movimiento = 1
  
