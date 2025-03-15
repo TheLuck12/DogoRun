05c860031123079677fa742777e2c9f37767607a
TheLuck12 <150361035+TheLuck12@users.noreply.github.com>
2025-03-15 13:39:49 -0300

    agregue animacion de saltar

--- Scripts/jugador.gd
+++ Scripts/jugador.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-
+@onready var animacion = $AnimationPlayer
 var salte : int
 @export var fuerzaSalto : int
-
 func _ready() -> void:
-	$AnimationPlayer.play("Correr")
+	Animaciones()
+
 func _physics_process(delta: float) -> void:
 	if Global.enJuego == true:
 		Gravedad(delta)
@@ -24,18 +24,27 @@ func Saltar():
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and salte == 0:
 		velocity.y += -fuerzaSalto
 		salte += 1
+		Animaciones()
 	else:
 		Caida()
 	MaxAltura()
 	
 func Mover():
-	velocity.x = 500 
+	velocity.x = 500
 func Caida():
 	if Input.is_action_just_released("ui_accept") and not is_on_floor() and salte == 1:
 		velocity.y = 400
 		salte += 1 
 	elif is_on_floor() and salte != 0:
 		salte = 0
+		Animaciones()
+
 func MaxAltura():
 	if velocity.y <= -800:
 		velocity.y = -800
+
+func Animaciones():
+	if salte == 0:
+		animacion.play("Correr")
+	elif salte != 0:
+		animacion.play("Saltar")
