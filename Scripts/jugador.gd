6c2927d2e73c68c52a8e309f535c0838d6718d5d
TheLuck12 <150361035+TheLuck12@users.noreply.github.com>
2025-03-18 23:13:30 -0300

    arregle codigo de animacion de salto

--- Scripts/jugador.gd
+++ Scripts/jugador.gd
@@ -1,53 +1,56 @@
 extends CharacterBody2D
 
-
 @onready var animacion = $AnimationPlayer
 var salte : int
 @export var fuerzaSalto : int
+
 func _ready() -> void:
 	Animaciones()
 
 func _physics_process(delta: float) -> void:
 	if Global.enJuego == true:
 		Gravedad(delta)
-		Saltar()
+		Movimiento()
 		Mover()
 		move_and_slide()
+		print(salte)
 	else:
 		$AnimationPlayer.stop()
 
 func Gravedad(Masa):
 	if not is_on_floor():
 		velocity += get_gravity() * Masa
-		Caida()
 
+func Movimiento():
+	Saltar()
+	Caida()
 func Saltar():
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and salte == 0:
+	if Input.is_action_just_pressed("Saltar") and is_on_floor() and salte == 0:
 		velocity.y += -fuerzaSalto
-		salte += 1
-		Animaciones()
-	else:
-		Caida()
-	MaxAltura()
-	
+		salte = 1
+		
+
+
 func Mover():
 	velocity.x = 500
+
 func Caida():
-	if Input.is_action_just_released("ui_accept") and not is_on_floor() and salte == 1:
-		velocity.y = 400
-		salte += 1 
-	elif is_on_floor() and salte != 0:
+	if Input.is_action_just_pressed("Caer") and not is_on_floor():
+		velocity.y = 700
+		salte = 2
+	if is_on_floor() and salte != 0:
 		salte = 0
 		Animaciones()
-	elif not is_on_floor():
+	elif salte != 2 and not is_on_floor():
 		salte = 1
 		Animaciones()
-func MaxAltura():
-	if velocity.y <= -800:
-		velocity.y = -800
+		
+		
+		
 
 func Animaciones():
 	if salte == 0:
 		animacion.play("Correr")
-	elif salte != 0 and not is_on_floor():
+	elif salte == 1 :
 		animacion.play("Saltar")
+		salte = 2
