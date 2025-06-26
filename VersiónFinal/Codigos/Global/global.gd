904531f9c83dcd882ba702c74f7855b82d3811f7
TheLuck12 <150361035+TheLuck12@users.noreply.github.com>
2025-06-26 16:29:40 -0300

    tui

--- VersiónFinal/Codigos/Global/global.gd
+++ VersiónFinal/Codigos/Global/global.gd
@@ -14,7 +14,3 @@ func Randomizar(MinNum, MaxNum):
 	rng.randomize()
 	return rng.randi_range(MinNum, MaxNum)
 	
-
-func _process(_delta: float) -> void:
-	if Input.is_key_pressed(KEY_Y):
-		get_tree().call_group("Interfaz", "ActPantalla")
